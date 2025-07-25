ce8ced9117f3712a6baa64696610ce04a26df21f
xrls9 <jagox28@outlook.com>
2025-07-25 09:33:50 -0500

    chore: integrated vehicle list views

--- apps/vehicles/src/app/ui/organisms/vehicle-table/vehicle-table-component.ts
+++ apps/vehicles/src/app/ui/organisms/vehicle-table/vehicle-table-component.ts
@@ -2,10 +2,11 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { Vehicle } from '@vehicle-insurance-monorepo/shared-utils';
+import { SharedButtonComponent } from '@vehicle-insurance-monorepo/shared-ui';
 
 @Component({
   selector: 'app-vehicle-table-component',
-  imports: [CommonModule],
+  imports: [CommonModule, SharedButtonComponent],
   templateUrl: './vehicle-table-component.html',
   styleUrl: './vehicle-table-component.css',
 })
@@ -15,6 +16,7 @@ export class VehicleTableComponent {
 
   @Output() editVehicle = new EventEmitter<Vehicle>();
   @Output() deleteVehicle = new EventEmitter<string>();
+  @Output() addVehicle = new EventEmitter<void>();
 
   hasVehicles(): boolean {
     return this.vehicles !== null && this.vehicles.length > 0;
@@ -28,5 +30,8 @@ export class VehicleTableComponent {
     if (confirm('¿Estás seguro de que quieres eliminar este vehículo?')) {
       this.deleteVehicle.emit(vehicleId);
     }
+  }
+  onAdd(): void {
+    this.addVehicle.emit();
   }
 }
